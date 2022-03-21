d72ff5c21b9c0d2d465c253a2991b20406f4a905
Alvaro A Lima Jr <alvaro.junior@grupoboticario.com.br>
2022-03-21 11:19:03 -0300

    feat(service): adding optional RfcCallOptions

--- lib/services/sap-service.interface.ts
+++ lib/services/sap-service.interface.ts
@@ -1,13 +1,15 @@
-import { RfcObject } from 'node-rfc';
+import { RfcCallOptions, RfcObject } from 'node-rfc';
 
 export interface SapService {
   /**
    *
    * @param rfcName
    * @param rfcParams
+   * @param options
    */
   execute<T extends RfcObject>(
     rfcName: string,
     rfcParams: RfcObject,
+    options?: RfcCallOptions,
   ): Promise<T>;
 }
