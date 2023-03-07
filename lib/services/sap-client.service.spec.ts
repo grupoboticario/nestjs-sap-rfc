09d9a3fa50d89c2cd62487d073ba4c5c7c1db635
Erlan Lucio  <67064886+lucioerlan@users.noreply.github.com>
2023-03-07 11:38:01 -0300

    test: adding new tests in the application (#650)

--- lib/services/sap-client.service.spec.ts
+++ lib/services/sap-client.service.spec.ts
@@ -28,15 +28,43 @@ describe('SapClientService', () => {
     sapClient = module.get<SapClient>(SAP_CLIENT);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(sapClient).toBeDefined();
   });
 
-  it('call RFC', async () => {
+  it('SAP_CLIENT is initialized correctly', () => {
+    expect(sapClient).toEqual(mockSapClient);
+  });
+
+  it('call RFC successfully', async () => {
     jest.spyOn(sapClient, 'call').mockResolvedValue(mockResult);
 
     expect(await service.execute(mockRFCName, mockParams)).toEqual(mockResult);
+    expect(sapClient.open).toHaveBeenCalledTimes(1);
+    expect(sapClient.call).toHaveBeenCalledTimes(1);
+  });
+
+  it('call RFC with valid parameters', async () => {
+    jest.spyOn(sapClient, 'call').mockResolvedValue(mockResult);
+
+    const result = await service.execute(mockRFCName, mockParams);
+    expect(result).toEqual(mockResult);
+    expect(sapClient.open).toHaveBeenCalledTimes(1);
+    expect(sapClient.call).toHaveBeenCalledTimes(1);
+  });
+
+  it('throws an error when call RFC fails', async () => {
+    jest.spyOn(sapClient, 'call').mockRejectedValue(new Error('Call failed'));
+
+    await expect(service.execute(mockRFCName, mockParams)).rejects.toThrow(
+      'Call failed',
+    );
+
     expect(sapClient.open).toHaveBeenCalledTimes(1);
     expect(sapClient.call).toHaveBeenCalledTimes(1);
   });
--- lib/services/sap-pool.service.spec.ts
+++ lib/services/sap-pool.service.spec.ts
@@ -29,15 +29,47 @@ describe('SapPoolService', () => {
     sapPool = module.get<SapPool>(SAP_POOL);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(sapPool).toBeDefined();
   });
 
-  it('call RFC', async () => {
+  it('SAP_POOL is initialized correctly', () => {
+    expect(sapPool).toEqual(mockSapPool);
+  });
+
+  it('call RFC successfully', async () => {
     jest.spyOn(mockSapClient, 'call').mockResolvedValue(mockResult);
 
     expect(await service.execute(mockRFCName, mockParams)).toEqual(mockResult);
+    expect(mockSapPool.acquire).toHaveBeenCalledTimes(1);
+    expect(mockSapClient.call).toHaveBeenCalledTimes(1);
+    expect(mockSapClient.release).toHaveBeenCalledTimes(1);
+  });
+
+  it('call RFC with valid parameters', async () => {
+    jest.spyOn(mockSapClient, 'call').mockResolvedValue(mockResult);
+
+    const result = await service.execute(mockRFCName, mockParams);
+    expect(result).toEqual(mockResult);
+    expect(mockSapPool.acquire).toHaveBeenCalledTimes(1);
+    expect(mockSapClient.call).toHaveBeenCalledTimes(1);
+    expect(mockSapClient.release).toHaveBeenCalledTimes(1);
+  });
+
+  it('throws an error when call RFC fails', async () => {
+    jest
+      .spyOn(mockSapClient, 'call')
+      .mockRejectedValue(new Error('Call failed'));
+
+    await expect(service.execute(mockRFCName, mockParams)).rejects.toThrow(
+      'Call failed',
+    );
+
     expect(mockSapPool.acquire).toHaveBeenCalledTimes(1);
     expect(mockSapClient.call).toHaveBeenCalledTimes(1);
     expect(mockSapClient.release).toHaveBeenCalledTimes(1);
